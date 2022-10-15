245ae79a4bff714760611c21207bcaf8bf6fead1
sbkimxtheia <murumuru4633@gmail.com>
2022-10-15 22:18:56 +0900

    Initial commit

---
+++ src/main/java/kr/sbxt/theia/theiacore/TheiaCore.java
@@ -0,0 +1,20 @@
+package kr.sbxt.theia.theiacore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TheiaCore extends JavaPlugin
+{
+	
+	@Override
+	public void onEnable()
+	{
+		// Plugin startup logic
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		// Plugin shutdown logic
+	}
+}
