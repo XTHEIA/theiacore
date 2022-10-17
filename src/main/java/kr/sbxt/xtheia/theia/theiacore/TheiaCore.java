47f228a9c8d922434d0b518e6d59e8ef583b1835
sbkimxtheia <murumuru4633@gmail.com>
2022-10-18 05:16:58 +0900

    Cleaned

---
+++ src/main/java/kr/sbxt/xtheia/theia/theiacore/TheiaCore.java
@@ -0,0 +1,19 @@
+package kr.sbxt.xtheia.theia.theiacore;
+
+/**
+ * THEIA Core
+ * XTHEIA Organization 산하 THEIA 프로젝트의 모든 Java Project의 코어 라이브러리
+ */
+public final class TheiaCore
+{
+	/**
+	 * GitHub Organization URL of XTHEIA
+	 */
+	public static String
+			
+			ORG_GITHUB_URL = "https://github.com/XTHEIA",
+	/**
+	 * GitHub Packages URL of this Library
+	 */
+			PACKAGES_URL = "https://github.com/XTHEIA/theiacore/packages/1676036";
+}
